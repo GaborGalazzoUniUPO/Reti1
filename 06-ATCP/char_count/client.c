7c7adb55f0dd131f9fb7767e97856b076de3f1dd
20024195 <20024195@studenti.uniupo.it>
2019-05-02 15:25:08 +0200

    TCP Avanzato

---
+++ 06-ATCP/char_count/client.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+
+    int simpleSocket = 0;
+    int simplePort = 0;
+    int returnStatus = 0;
+    char buffer[256] = "";
+    struct sockaddr_in simpleServer;
+
+    int x;
+
+    if (4 != argc)
+    {
+
+        fprintf(stderr, "Usage: %s <server> <port> <x>\n", argv[0]);
+        exit(1);
+    }
+
+    x = atoi(argv[3]);
+
+    /* create a streaming socket      */
+    simpleSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (simpleSocket == -1)
+    {
+
+        fprintf(stderr, "Could not create a socket!\n");
+        exit(1);
+    }
+    else
+    {
+        fprintf(stderr, "Socket created!\n");
+    }
+
+    /* retrieve the port number for connecting */
+    simplePort = atoi(argv[2]);
+
+    /* setup the address structure */
+    /* use the IP address sent as an argument for the server address  */
+    //bzero(&simpleServer, sizeof(simpleServer));
+    memset(&simpleServer, '\0', sizeof(simpleServer));
+    simpleServer.sin_family = AF_INET;
+    //inet_addr(argv[2], &simpleServer.sin_addr.s_addr);
+    simpleServer.sin_addr.s_addr = inet_addr(argv[1]);
+    simpleServer.sin_port = htons(simplePort);
+
+    /*  connect to the address and port with our socket  */
+    returnStatus = connect(simpleSocket, (struct sockaddr *)&simpleServer, sizeof(simpleServer));
+
+    if (returnStatus == 0)
+    {
+        fprintf(stderr, "Connect successful!\n");
+    }
+    else
+    {
+        perror("Could not connect to address!\n");
+        close(simpleSocket);
+        exit(1);
+    }
+
+    
+
+
+     returnStatus = write(simpleSocket, argv[3], strlen(argv[3]));
+        if (returnStatus <0)
+        {
+            perror("Invio della quantita fallito");
+            close(simpleSocket);
+            exit(1);
+        }
+
+
+    printf("Attendo %d messaggi...\n", x);
+
+    for (int i = 0; i < x; i++)
+    {
+        printf("Messaggio %d > ", i);
+        returnStatus = scanf("%s", buffer);
+         if (returnStatus != 1)
+        {
+            perror("Errore lettura stdin");
+            close(simpleSocket);
+            exit(1);
+        }
+
+        returnStatus = write(simpleSocket, buffer, strlen(buffer));
+        if (returnStatus > 0)
+        {
+            printf("%d: invio del messaggio %d\n", returnStatus, i);
+        }
+        else
+        {
+            fprintf(stderr, "Return Status = %d \n", returnStatus);
+            close(simpleSocket);
+            exit(1);
+        }
+
+        int resp;
+
+        returnStatus = read(simpleSocket, &resp, sizeof(int));
+
+        if (returnStatus > 0)
+        {
+            printf("%d: risposta %d - %d\n", returnStatus, i, resp);
+        }
+        else
+        {
+            fprintf(stderr, "Return Status = %d \n", returnStatus);
+            close(simpleSocket);
+            exit(1);
+        }
+    }
+
+    double serviceTime;
+
+        returnStatus = read(simpleSocket, &serviceTime, sizeof(double));
+
+        if (returnStatus > 0)
+        {
+            printf("Tempo impiegato: %.2lf secondi\n", serviceTime);
+        }
+        else
+        {
+            fprintf(stderr, "Return Status = %d \n", returnStatus);
+            close(simpleSocket);
+            exit(1);
+        }
+
+    close(simpleSocket);
+
+    return 0;
+}
